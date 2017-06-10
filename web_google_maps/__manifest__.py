b1dc4670cfc90b8e53ad26b92f8148a743ae2962
Yopi Angi <yopiangi@Yopis-Mac-mini.local>
2017-06-10 14:13:53 +0700

    Update version to v1.1

--- web_google_maps/__manifest__.py
+++ web_google_maps/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Web Google Maps',
-    'version': '1.0',
+    'version': '1.1',
     'author': "Yopi Angi",
     'maintainer': 'Yopi Angi<yopiangi@gmail.com>',
     'category': 'web',
