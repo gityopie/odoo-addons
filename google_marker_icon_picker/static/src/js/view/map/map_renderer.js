614f5197f59a2ec07f9fb0d90ca0d1eb441164c4
Yopi Angi <yopiangi@gmail.com>
2019-03-31 19:49:16 +0700

    - Moved the changed for iconUrl to _initLibraryProperties

--- google_marker_icon_picker/static/src/js/view/map/map_renderer.js
+++ google_marker_icon_picker/static/src/js/view/map/map_renderer.js
@@ -4,14 +4,11 @@ odoo.define('google_marker_dynamic_color.MapRenderer', function (require) {
     var MapRenderer = require('web_google_maps.MapRenderer');
 
     MapRenderer.include({
-        init: function (parent, state, params) {
-            this._super.apply(this, arguments);
-            this.iconUrl = '/google_marker_icon_picker/static/src/img/markers/';
-        },
         _initLibraryProperties: function (params) {
             this._super.apply(this, arguments);
             if (this.mapLibrary === 'geometry') {
                 this.fieldMarkerColor = params.fieldMarkerColor;
+                this.iconUrl = '/google_marker_icon_picker/static/src/img/markers/';
             }
         },
         _createMarker: function (latLng, record, color) {
