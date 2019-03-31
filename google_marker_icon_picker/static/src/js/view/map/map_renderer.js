2b38338c330585ec7fb9f47c0876e4afda80b25f
Yopi Angi <yopiangi@gmail.com>
2019-03-31 19:22:09 +0700

    * Contacts Maps  - Define new field marker_color on res.partner  - Defined new view for map view on Contacts * Web Google Maps Drawing  - Update the sample module  - Improved widget map_drawing_shape, especially for the function that manage to center the map * Google Marker Icon Picker [New module]  - Defined new widget google_marker_picker

---
+++ google_marker_icon_picker/static/src/js/view/map/map_renderer.js
@@ -0,0 +1,23 @@
+odoo.define('google_marker_dynamic_color.MapRenderer', function (require) {
+    'use strict';
+
+    var MapRenderer = require('web_google_maps.MapRenderer');
+
+    MapRenderer.include({
+        init: function (parent, state, params) {
+            this._super.apply(this, arguments);
+            this.iconUrl = '/google_marker_icon_picker/static/src/img/markers/';
+        },
+        _initLibraryProperties: function (params) {
+            this._super.apply(this, arguments);
+            if (this.mapLibrary === 'geometry') {
+                this.fieldMarkerColor = params.fieldMarkerColor;
+            }
+        },
+        _createMarker: function (latLng, record, color) {
+            var color = (record.data[this.fieldMarkerColor] ? record.data[this.fieldMarkerColor] : color) || 'red';
+            this._super(latLng, record, color);
+        },
+    });
+
+});
