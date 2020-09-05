6ddc8808a947c17f3f670154e69cd86063b025e4
Yopi Angi <yopiangi@gmail.com>
2020-09-05 15:42:10 +0700

    Bump google_marker_icon_picker version

--- google_marker_icon_picker/__manifest__.py
+++ google_marker_icon_picker/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Google Marker Icon Picker',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'author': 'Yopi Angi',
     'license': 'AGPL-3',
     'maintainer': 'Yopi Angi<yopiangi@gmail.com>',
